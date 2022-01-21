454a0d0e6c9769cd1318686ecfa0af7fce8f7377
Eric Schultz <ericschultz101@gmail.com>
2022-01-21 12:19:22 -0800

    Add macro templates pack

--- scripts/render-draggable-rolltable-result.js
+++ scripts/render-draggable-rolltable-result.js
@@ -5,19 +5,20 @@ async function getItemFromTableRoll(tableResult) {
 }
 
 async function getDraggableItemCardContent(itm) {
-   const templateData = {
-     item: itm.data,
-     data: itm.getChatData(),
-     labels: itm.labels,
-     hasAttack: itm.hasAttack,
-     isHealing: itm.isHealing,
-     hasDamage: itm.hasDamage,
-     isVersatile: itm.isVersatile,
-     isSpell: itm.data.type === "spell",
-     hasSave: itm.hasSave,
-     hasAreaTarget: itm.hasAreaTarget,
-     isTool: itm.data.type === "tool",
-     hasAbilityCheck: itm.hasAbilityCheck
-   };
+  const templateData = {
+    item: itm.data,
+    data: itm.getChatData(),
+    labels: itm.labels,
+    hasAttack: itm.hasAttack,
+    isHealing: itm.isHealing,
+    hasDamage: itm.hasDamage,
+    isVersatile: itm.isVersatile,
+    isSpell: itm.data.type === "spell",
+    hasSave: itm.hasSave,
+    hasAreaTarget: itm.hasAreaTarget,
+    isTool: itm.data.type === "tool",
+    hasAbilityCheck: itm.hasAbilityCheck
+  };
+  
   return await renderTemplate("modules/multi-rolltable-cards/item-card-draggable.html", templateData);
 }
